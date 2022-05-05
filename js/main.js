fdaa430814743f011c9d5f3e7c3cfcea1dea4dc3
Diwas Poudel <n_diwas.poudel@cashdirector.com>
2022-05-05 13:37:30 +0545

    new chnages

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 (function ($) {
   $('.toggle_btn').on('click', function (e) {
     e.preventDefault();
-    console.log($('.toggle_Box').hasClass('active'), "From ACtive class");
+    console.log($('.toggle_Box').hasClass('active'), 'From ACtive class');
     var toggle = $(this).add('.toggle_Box');
     toggle.toggleClass('active');
   });
@@ -19,18 +19,40 @@
       var activeBlock = parent.find('.testinomial_container.active');
       var currentPos = $(this).attr('data-position');
       var newPos = activeBlock.attr('data-position');
-      console.log(newPos, "From current pos");
-      activeBlock.removeClass('active').removeClass(newPos).addClass('inactive').addClass(currentPos);
+      console.log(newPos, 'From current pos');
+      activeBlock
+        .removeClass('active')
+        .removeClass(newPos)
+        .addClass('inactive')
+        .addClass(currentPos);
       activeBlock.attr('data-position', currentPos);
-      $(this).addClass('active').removeClass('inactive').removeClass(currentPos).addClass(newPos);
+      $(this)
+        .addClass('active')
+        .removeClass('inactive')
+        .removeClass(currentPos)
+        .addClass(newPos);
       $(this).attr('data-position', newPos);
     }
   });
+  var alterClass = function () {
+    var ww = document.body.clientWidth;
+    if (ww < 900) {
+      $('.we_serve_slider').addClass('weServe owl-carousel owl-theme');
+    } else if (ww >= 901) {
+      $('.we_serve_slider').removeClass('weServe owl-carousel owl-theme');
+    }
+  };
+  $(window).resize(function () {
+    alterClass();
+  });
+  //Fire it when the page first loads:
+  alterClass();
 })(jQuery);
 
 $(document).ready(function () {
   var owl = $('.services');
-  var blog = $('#blogs')
+  var blog = $('#blogs');
+  var weServe = $('.weServe');
   owl.owlCarousel({
     items: 4,
     // items change number for slider display on desktop
@@ -46,14 +68,45 @@ $(document).ready(function () {
     autoplay: false,
     nav: true,
   });
-});
-
-$(document).ready(function () {
-
-
-  $("#tabClass .nav-link").click(function (event) {
-    $("#tabClass .nav-link").removeClass("active");
-    $(this).removeClass('active');
+  weServe.owlCarousel({
+    margin: 10,
+    dots: false,
+    nav: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      600: {
+        items: 4,
+      },
+      750: {
+        items: 5,
+      },
+      900: {
+        items: 6,
+      },
+    },
   });
 });
 
+// jQuery(document).ready(function ($) {
+//   var alterClass = function () {
+//     var ww = document.body.clientWidth;
+//     if (ww < 600) {
+//       $('.we_serve_slider').addClass('weServe owl-carousel owl-theme');
+//     } else if (ww >= 601) {
+//       $('.we_serve_slider').removeClass('weServe');
+//     }
+//   };
+//   $(window).resize(function () {
+//     alterClass();
+//   });
+//   alterClass();
+// });
+
+$(document).ready(function () {
+  $('#tabClass .nav-link').click(function (event) {
+    $('#tabClass .nav-link').removeClass('active');
+    $(this).removeClass('active');
+  });
+});
